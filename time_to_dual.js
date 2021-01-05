9b79d873eee7680c5c1124a9661542d1f7a53721
Enoch <enochstrok@outlook.com>
2021-01-04 20:56:08 -0800

    first commit on project

---
+++ time_to_dual.js
@@ -0,0 +1,70 @@
+/////////// Card Class - Creation ////////////////////////////////////////////////////////////
+class Card {
+    constructor(name, cost){
+        this.name = name;
+        this.cost = cost;
+    }
+}
+
+/////////// Child Card Class - Unit - Creation ////////////////////////////////////////////////////////////
+class Unit extends Card {
+    constructor(name, cost, power, res){
+        super(name, cost);
+        this.power = power;
+        this.res = res;
+    }
+    attack_res(target){
+        target.res = target.res - this.power;
+    }
+}
+
+/////////// Child Card Class - Effect - Creation ////////////////////////////////////////////////////////////
+class Effect extends Card {
+    constructor(name, cost, text, stat, magnitude){
+        super(name, cost);
+        this.stat = stat;
+        this.magnitude = magnitude;
+        this.text = text;
+    }
+    play( target ) {
+        if( target instanceof Unit ) {
+            if(this.stat === 'res'){
+                console.log(`${this.text} against ${target.name}`);
+                target.res += this.magnitude;
+            }else if(this.stat === 'power'){
+                console.log(`${this.text} against ${target.name}`);
+                target.power += this.magnitude;
+            }   
+        } else {
+            throw new Error( "Target must be a unit!" );
+        }
+    }
+}
+/////////// Red Belt Card - Creation ////////////////////////////////////////////////////////////
+const redBeltCard = new Unit("Red Belt Ninja", 3, 3, 4);
+console.log(redBeltCard);
+/////////// Buff Effect - Created
+const hardAlgo = new Effect("Hard Algorithm", 2, "increase target's resilience by 3", "res", +3)
+// console.log(hardAlgo);
+/////////// Buff Effect - Played
+hardAlgo.play(redBeltCard);
+console.log(redBeltCard);
+
+/////////// Black Belt Card - Creation ////////////////////////////////////////////////////////////
+const blackBeltCard = new Unit("Black Belt Ninja", 4, 5, 4);
+console.log(blackBeltCard);
+/////////// Nerf Effect - Creation
+const unhandledPromiseRejection = new Effect("Unhandled Promise Rejection", 1, "reduce target's resilience by 2", "res", -2)
+// console.log(unhandledPromiseRejection);
+/////////// Nerf Effect - Played
+unhandledPromiseRejection.play(blackBeltCard);
+console.log(blackBeltCard);
+
+
+/////////// Nerf Effect - Creation ////////////////////////////////////////////////////////////
+const pairProgramming = new Effect("Pair Programming", 3, "increase target's power by 2", "power", +2)
+// console.log(pairProgramming);
+/////////// Nerf Effect - Played
+pairProgramming.play(redBeltCard);
+console.log(redBeltCard);
+
